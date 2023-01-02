3a47933be869c34b6eb7dd8d8cf8e0ab260ae492
kimheeseok <kim1140129@gmail.com>
2023-01-03 02:01:38 +0900

    <FEAT>: POST API CRUD 생성

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -7,9 +7,49 @@ router.post('/',async (req:Request, res:Response) => {
     try {
         const {title,content} = req.body;
         await Posts.create({title, content});
+
+        res.json({message: "등록되었습니다."})
     }catch (error){
         console.log(error);
     }
 });
 
+router.get('/',async (req:Request, res:Response) => {
+    try {
+        const result = await Posts.findAll({});
+        res.json({data:result});
+
+    }catch (error){
+        console.log(error);
+    }
+});
+
+router.delete('/:postId', async (req:Request, res:Response) => {
+    try {
+        const {postId} = req.params;
+        await Posts.destroy({
+            where:{postId}
+        });
+        res.json({data:"삭제되었습니다."});
+    }catch (error){
+        console.log(error);
+    }
+});
+
+router.put('/:postId', async (req:Request, res:Response) => {
+    try {
+        const {postId} = req.params;
+        const {title,content} = req.body;
+
+        await Posts.update(
+            { title,content },
+            {
+                where: {postId},
+            }
+        )
+        res.json({data:"수정되었습니다."});
+    }catch (error){
+        console.log(error);
+    }
+});
 export default router;
