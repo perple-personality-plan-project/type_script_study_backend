a9b8a563c44f9e9c82b06e9266f37b789360b740
kimheeseok <kim1140129@gmail.com>
2023-01-03 17:49:23 +0900

    <FIX>: CORS 누락 배포

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import * as express from "express";
 import routes from "./routes";
 import sequelize from "./models/index";
-import cors from 'cors';
+import * as cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors({ exposedHeaders: ['Authorization'] }));
+app.use(cors());
 
 app.use("/api", routes);
 
